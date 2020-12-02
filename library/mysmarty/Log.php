ca341d05caa5ff849399b56dd1555fe089f9f911
daiji <1445023846@qq.com>
2020-12-02 16:38:36 +0800

    日志类

--- library/mysmarty/Log.php
+++ library/mysmarty/Log.php
@@ -4,38 +4,29 @@ namespace library\mysmarty;
 
 /**
  * 日志类
- *
-
- *
  */
 class Log
 {
 
     /**
      * 写入日志
-     *
-     * @param string $info
-     *            消息
-     * @param string $type
-     *            类型
+     * @param string $info 消息
+     * @param string $type 类型
      *            log 常规日志，用于记录日志
      *            error 错误，一般会导致程序的终止
      *            notice 警告，程序可以运行但是还不够完美的错误
      *            info 信息，程序输出信息
      *            debug 调试，用于调试信息
      *            sql SQL语句，用于SQL记录，只在数据库的调试模式开启时有效
-     * @throws \Exception
      */
-    public static function record($info, $type = 'record')
+    public static function record(string $info, string $type = 'record'): void
     {
         $dir = RUNTIME_DIR . '/logs';
-        if (!file_exists($dir)) {
-            mkdir($dir, 0777, true);
-        }
+        createDir($dir);
         $file = $dir . '/' . date('Ymd') . '.txt';
         $handle = fopen($file, 'a+');
         if (!$handle) {
-            throw new \RuntimeException('log文件打开失败！');
+            exit('log文件打开失败！');
         }
         $space = '';
         if (mb_strlen($type, 'utf-8') < 10) {
@@ -47,66 +38,54 @@ class Log
 
     /**
      * 常规日志，用于记录日志
-     *
      * @param string $info
-     * @throws \Exception
      */
-    public static function log($info)
+    public static function log(string $info): void
     {
         self::record($info, 'log');
     }
 
     /**
      * 错误，一般会导致程序的终止
-     *
      * @param string $info
-     * @throws \Exception
      */
-    public static function error($info)
+    public static function error(string $info): void
     {
         self::record($info, 'error');
     }
 
     /**
      * 警告，程序可以运行但是还不够完美的错误
-     *
      * @param string $info
-     * @throws \Exception
      */
-    public static function notice($info)
+    public static function notice(string $info): void
     {
         self::record($info, 'notice');
     }
 
     /**
      * 信息，程序输出信息
-     *
      * @param string $info
-     * @throws \Exception
      */
-    public static function info($info)
+    public static function info(string $info): void
     {
         self::record($info, 'info');
     }
 
     /**
      * 调试，用于调试信息
-     *
      * @param string $info
-     * @throws \Exception
      */
-    public static function debug($info)
+    public static function debug(string $info): void
     {
         self::record($info, 'debug');
     }
 
     /**
      * SQL语句，用于SQL记录
-     *
      * @param string $sql
-     * @throws \Exception
      */
-    public static function sql($sql)
+    public static function sql(string $sql): void
     {
         self::record($sql, 'sql');
     }
@@ -114,7 +93,7 @@ class Log
     /**
      * 清空日志
      */
-    public static function clear()
+    public static function clear(): void
     {
         $dir = RUNTIME_DIR . '/logs';
         if (!file_exists($dir)) {
